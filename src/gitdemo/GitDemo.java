ea209eeb93d0ddaa4a8d90a0bbf3acd7f31aee89
John Doe <johndoe@example.com>
2015-10-07 11:31:29 -0300

    Ajout nouvelle ligne de commande

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -17,7 +17,8 @@ public class GitDemo {
     public static void main(String[] args) {
        
         System.out.println ("Bonjour John");
-               
+        
+        System.out.println ("nouvelle modification");
     }
     
 }
